1faf5c049b171e56bbcda130bc864fbbb42eeb12
kamran <khanbrother805@gmail.com>
2021-06-25 03:42:48 +0530

    Custom-Counting

--- API/urls.py
+++ API/urls.py
@@ -9,13 +9,13 @@ router = DefaultRouter()
 router.register('stream', StreamPlatformVS, basename='streamplatform')
 urlpatterns = [
     path('list/',WatchListAV.as_view(),name='movie-list'),
-    path('list/<int:pk>',WatchDetailAV.as_view(),name='movie-detail'),
+    path('<int:pk>',WatchDetailAV.as_view(),name='movie-detail'),
 
 
     path('',include(router.urls)),
-    path('stream/<int:pk>/review-create',ReviewCreate.as_view(),name='review-create'),
-    path('stream/<int:pk>/review',ReviewList.as_view(),name='review-list'),
-    path('stream/review/<int:pk>',ReviewDetail.as_view(),name='review-detail'),
+    path('<int:pk>/review-create',ReviewCreate.as_view(),name='review-create'),
+    path('<int:pk>/review',ReviewList.as_view(),name='review-list'),
+    path('review/<int:pk>',ReviewDetail.as_view(),name='review-detail'),
 
 
 
--- API/views.py
+++ API/views.py
@@ -42,6 +42,18 @@ class ReviewCreate(generics.CreateAPIView):
 
         if review_queryset.exists():
             raise ValidationError('You have already reviewed this movie!!')
+
+        if watchlist.number_of_ratiing == 0:
+            watchlist.avg_rating = serializer.validated_data['rating']
+        else:
+            watchlist.avg_rating = (watchlist.avg_rating + serializer.validated_data['rating'])/2
+
+        watchlist.number_of_ratiing += 1
+        watchlist.save()
+
+
+
+
         serializer.save(watchlist=watchlist,review_user=review_user)
 
 
